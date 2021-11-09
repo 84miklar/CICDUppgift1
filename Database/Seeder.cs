47d6d2315477749df60da613041fd5c90c9ef1bd
Mikael Larsson <73987306+84miklar@users.noreply.github.com>
2021-11-09 08:38:44 +0100

    filled some users

--- Database/Seeder.cs
+++ Database/Seeder.cs
@@ -12,6 +12,11 @@ namespace CICDUppgift1.Database
         public static void AddNewUsers()
         {
             FillUser("admin1", "admin1234", 1000, "admin", true);
+            FillUser("user1", "123", 2000, "IT", false);
+            FillUser("user2", "123", 2000, "IT", false);
+            FillUser("user3", "123", 2000, "IT", false);
+            FillUser("user4", "123", 2000, "IT", false);
+
         }
         public static void FillUser(string name, string password, int salary, string title, bool isAdmin)
         {
