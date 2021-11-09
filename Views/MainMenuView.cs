5e93929c5bcefbfb1e40d2ebbfa9d71579bf7193
Mikael Larsson <73987306+84miklar@users.noreply.github.com>
2021-11-09 10:29:21 +0100

    Started adminmenu

--- Views/MainMenuView.cs
+++ Views/MainMenuView.cs
@@ -16,6 +16,46 @@ namespace CICDUppgift1.Views
 
         internal void MainMenu(User loggedInUser)
         {
+            Console.Clear();
+            
+            if (!loggedInUser.IsAdmin)
+            {
+            UserMenuSwitch(loggedInUser);
+            }
+            else
+            {
+                AdminMenuSwitch(loggedInUser);
+            }
+        }
+
+        private void AdminMenuSwitch(User loggedInUser)
+        {
+            Console.WriteLine("1. Show salary \n2.Show title \n3. Handle users \n 4. Exit");
+            var userChoice = check.TryParse();
+            switch (userChoice)
+            {
+                case 1:
+                    ShowSalary(loggedInUser);
+                    break;
+
+                case 2:
+                    ShowTitle(loggedInUser);
+                    break;
+
+                case 3:
+                    DeleteUser(loggedInUser);
+                    break;
+
+                case 4:
+                    return;
+
+                default:
+                    break;
+            }
+        }
+
+        private void UserMenuSwitch(User loggedInUser)
+        {
             Console.WriteLine("1. Show salary \n2.Show title \n3. Delete yourself \n 4. Exit");
             var userChoice = check.TryParse();
             switch (userChoice)
@@ -48,11 +88,13 @@ namespace CICDUppgift1.Views
         private void ShowTitle(User loggedInUser)
         {
             Console.WriteLine($"Your title is {loggedInUser.Title}");
+            check.PressEnter();
         }
 
         private void ShowSalary(User loggedInUser)
         {
             Console.WriteLine($"Your salary is {loggedInUser.Salary}");
+            check.PressEnter();
         }
     }
 }
