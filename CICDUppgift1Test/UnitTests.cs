f5f4ee081068c8e4cce71502f68a9e8bf5710e37
LinNes <lnestorson@gmail.com>
2021-11-16 09:33:28 +0100

    Added integrations tests

--- CICDUppgift1Test/UnitTests.cs
+++ CICDUppgift1Test/UnitTests.cs
@@ -24,6 +24,7 @@
             var returnedUser = loginContr.Login(username, password);
             Assert.AreEqual(returnedUser.Name, username);
         }
+
         [Test]
         [TestCase("use1", "testpass1")]
         [TestCase("user1", "testpass")]
@@ -59,13 +60,36 @@
 
         [Test]
         [TestCase("user2", "testpass2")]
-        public void DeleteUserTest(string username, string password)
+        public void UserDeleteUserTest(string username, string password)
         {
             UserDatabase context = new();
             var userContr = new UserMenuController();
             var user = context.Users.FirstOrDefault(u => u.Name == username && u.Password == password);
             var returnedUser = userContr.DeleteUser(user);
             Assert.IsFalse(returnedUser);
+        }
+
+        [Test]
+        [TestCase("user3", "testpass3")]
+        public void AdminDeleteUserTest(string username, string password)
+        {
+            UserDatabase context = new();
+            var adminContr = new AdminMenuController();
+            adminContr.DeleteUser(username, password);
+            var user = context.Users.FirstOrDefault(u => u.Name == username && u.Password == password);
+            Assert.IsNull(user);
+        }
+
+        [Test]
+        [TestCase("Testperson 2", "TestPass123", 25000, "CEO", false)]
+        [TestCase("", "TestPass123", 25000, "CEO", false)]
+        public void AddUserTest_NullExpected(string name, string password, int salary, string title, bool isAdmin)
+        {
+            var adminContr = new AdminMenuController();
+            adminContr.AddUser(name, password, salary, title, isAdmin);
+            UserDatabase context = new();
+            var user = context.Users.FirstOrDefault(u => u.Name == name && u.Password == password);
+            Assert.IsNull(user);
         }
 
         [Test]
@@ -77,17 +101,6 @@
             var listOfUsers = adminContr.ShowAllUsers();
             var userResult = listOfUsers.FirstOrDefault(x => x.Name == name) as User;
             Assert.AreEqual(userResult.Name, name);
-        }
-        [Test]
-        [TestCase("Testperson 2", "TestPass123", 25000, "CEO", false)]
-        [TestCase("", "TestPass123", 25000, "CEO", false)]
-        public void AddUserTest_NullExpected(string name, string password, int salary, string title, bool isAdmin)
-        {
-            var adminContr = new AdminMenuController();
-            adminContr.AddUser(name, password, salary, title, isAdmin);
-            UserDatabase context = new();
-            var user = context.Users.FirstOrDefault(u => u.Name == name && u.Password == password);
-            Assert.IsNull(user);
         }
     }
 }
